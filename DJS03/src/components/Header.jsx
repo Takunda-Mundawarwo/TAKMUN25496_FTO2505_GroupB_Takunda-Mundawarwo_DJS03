8590480f2383aaa1a2723c2100556747f57f911a
Takunda Mundawarwo <tkmundawarwo@gmail.com>
2025-09-26 11:30:28 +0200

    Add podcast searching funcitonality
    Add search input to Header component. Add searchQuery state and implemement filtering of podcasts based on searchQuery state in Home component

--- DJS03/src/components/Header.jsx
+++ DJS03/src/components/Header.jsx
@@ -1,17 +1,29 @@
 import Logo from "../assets/echo-logo.svg";
+import "../styles/Header.css";
 
-const headerStyles = {
-  fontStyle: "italic",
-  display: "flex",
-  justifyContent: "start",
-  alignItems: "center",
-};
-
-export default function Header() {
+/**
+ * The header component, containing the App logo and search input.
+ *
+ * @param {object} props - The component props
+ * @param {string} props.searchQuery - The current search query state
+ * @param {function} props.setSearchQuery - The state setter function for the search query state.
+ * @returns {JSX.Element} - The rendered Header
+ */
+export default function Header({ searchQuery, setSearchQuery }) {
   return (
-    <header style={headerStyles}>
-      <img src={Logo} alt="Echo Logo" width="48" />
-      <h1>Echo</h1>
+    <header>
+      <div className="header-title">
+        <img src={Logo} alt="Echo Logo" width="48" />
+        <h1>Echo</h1>
+      </div>
+      <div className="header-filters">
+        <input
+          type="search"
+          className="search"
+          value={searchQuery}
+          onChange={setSearchQuery}
+        />
+      </div>
     </header>
   );
 }
--- DJS03/src/pages/Home.jsx
+++ DJS03/src/pages/Home.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import Header from "../components/Header.jsx";
 import PodcastList from "../components/PodcastList.jsx";
 
+/**
+ * A component that displays the podcast app's landing page, with the ability to search for podcasts
+ *
+ * @component
+ * @example <caption>Basic Usage</caption>
+ * <Home podcasts={podcasts} />
+ *
+ * @param {Array<Podcast>} - The complete array of podcasts available
+ * @returns {JSX.Element} - The Landing/Home page
+ */
+
 export default function Home({ podcasts }) {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredPodcasts = podcasts.filter((podcast) =>
+    podcast.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <>
-      <Header />
-      <PodcastList data={podcasts} />
+      <Header
+        searchQuery={searchQuery}
+        setSearchQuery={(e) => setSearchQuery(e.target.value)}
+      />
+      <PodcastList data={filteredPodcasts} />
     </>
   );
 }
