ba163398c40ce3029ac73fcc42a6be473bf79645
Takunda Mundawarwo <tkmundawarwo@gmail.com>
2025-09-26 13:12:34 +0200

    Add sorting functionality
    Add sorting select element to Header component. Add sortOrder state to and sorting methods for each sorting state in Home component

--- DJS03/src/components/Header.jsx
+++ DJS03/src/components/Header.jsx
@@ -3,21 +3,38 @@ import "../styles/Header.css";
 import { genres } from "../data.js";
 
 /**
- * The header component, containing the App logo and search input.
+ * The header component, containing the App logo, search, genre, and sort inputs.
+ *
+ * @component
+ * @example <caption>Standard Usage</caption>
+ * <Header
+ * searchQuery={searchQuery}
+ * setSearchQuery={(e) => setSearchQuery(e.target.value)}
+ * genreFilter={genreFilter}
+ * setGenreFilter={(e) => setGenreFilter(e.target.value)}
+ * sortOrder={sortOrder}
+ * setSortOrder={(e) => setSortOrder(e.target.value)}
+ * />
  *
  * @param {object} props - The component props
  * @param {string} props.searchQuery - The current search query state
  * @param {function} props.setSearchQuery - The state setter function for the search query state.
  * @param {string} props.selectedGenre - The current selected genre state
  * @param {function} props.setSelectedGenre - The state setter function for the selected genre state.
+ * @param {string} props.sortOrder - The sorting order state
+ * @param {function} props.setSortOrder - The setter function for the sortOrder state
  * @returns {JSX.Element} - The rendered Header
  */
-export default function Header({
-  searchQuery,
-  setSearchQuery,
-  genreFilter,
-  setGenreFilter,
-}) {
+export default function Header(props) {
+  const {
+    searchQuery,
+    setSearchQuery,
+    genreFilter,
+    setGenreFilter,
+    sortOrder,
+    setSortOrder,
+  } = props;
+
   return (
     <header>
       <div className="header-title">
@@ -43,6 +60,11 @@ export default function Header({
               {genre.title}
             </option>
           ))}
+        </select>
+        <select name="sortOrder" value={sortOrder} onChange={setSortOrder}>
+          <option value="newest">Newest First</option>
+          <option value="a-z">Sort A-Z</option>
+          <option value="z-a">Sort Z-A</option>
         </select>
       </div>
     </header>
--- DJS03/src/pages/Home.jsx
+++ DJS03/src/pages/Home.jsx
@@ -3,7 +3,8 @@ import Header from "../components/Header.jsx";
 import PodcastList from "../components/PodcastList.jsx";
 
 /**
- * A component that displays the podcast app's landing page, with the ability to search for podcasts
+ * A component that displays the podcast app's landing page, with the ability to search for podcasts,
+ * filter podcasts by genre and sort podcasts.
  *
  * @component
  * @example <caption>Basic Usage</caption>
@@ -15,12 +16,23 @@ import PodcastList from "../components/PodcastList.jsx";
 export default function Home({ podcasts }) {
   const [searchQuery, setSearchQuery] = useState("");
   const [genreFilter, setGenreFilter] = useState("All");
+  const [sortOrder, setSortOrder] = useState("newest");
 
+  //Filtering based on search query and selected genre filter
   const filteredPodcasts = podcasts.filter(
     (podcast) =>
       podcast.title.toLowerCase().includes(searchQuery.toLowerCase()) &&
       (podcast.genres.includes(Number(genreFilter)) || genreFilter === "All")
   );
+
+  //Sorting filtered podcasts according to selected sort order
+  filteredPodcasts.sort((podA, podB) => {
+    if (sortOrder === "newest") {
+      return new Date(podB.updated) - new Date(podA.updated);
+    } else if (sortOrder === "a-z") {
+      if (podA.title < podB.title) return -1;
+    } else if (podA.title > podB.title) return -1;
+  });
 
   return (
     <>
@@ -29,6 +41,8 @@ export default function Home({ podcasts }) {
         setSearchQuery={(e) => setSearchQuery(e.target.value)}
         genreFilter={genreFilter}
         setGenreFilter={(e) => setGenreFilter(e.target.value)}
+        sortOrder={sortOrder}
+        setSortOrder={(e) => setSortOrder(e.target.value)}
       />
       <PodcastList data={filteredPodcasts} />
     </>
